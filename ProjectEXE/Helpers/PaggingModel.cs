38cf3b457f35943758175c73324a820824cbccc4
hoa <hoaphung10112003@gmail.com>
2025-05-19 22:59:04 +0700

    pagging

---
+++ ProjectEXE/Helpers/PaggingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectEXE.Helpers
+{
+    public class PaggingModel
+    {
+        public int CurrentPage { get; set; }
+        public int CountPage { get; set; }
+        public Func<int?, string> GenerateUrl { get; set; }
+    }
+}
