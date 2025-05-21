5400f87e116676f20f201740393e72511100811b
hoa <hoaphung10112003@gmail.com>
2025-05-21 11:00:21 +0700

    Model zalo

---
+++ ProjectEXE/ViewModel/Zalo/ZaloPayEmbeddedData.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectEXE.ViewModel.Zalo
+{
+    public class ZaloPayEmbeddedData
+    {
+        public string UserId { get; set; }
+        public string PackageId { get; set; }
+    }
+}
