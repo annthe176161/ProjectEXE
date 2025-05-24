58a7db56293e24a9af4c66b6faa97452c9fa2143
hoa <hoaphung10112003@gmail.com>
2025-05-24 09:59:01 +0700

    delete zalo payment

--- ProjectEXE/ViewModel/Zalo/ZaloPayEmbeddedData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProjectEXE.ViewModel.Zalo
-{
-    public class ZaloPayEmbeddedData
-    {
-        public string UserId { get; set; }
-        public string PackageId { get; set; }
-    }
-}
